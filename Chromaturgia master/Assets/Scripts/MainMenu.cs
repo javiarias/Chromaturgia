43de333db9caca0d21369d13f0de599633224ab9
argarc03 <argarc03@ucm.es>
2018-04-10 23:53:48 +0200

    Terminado Menú Principal

--- Chromaturgia master/Assets/Scripts/MainMenu.cs
+++ Chromaturgia master/Assets/Scripts/MainMenu.cs
@@ -1,56 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
 	Animator thinkAnim;
 
-	GameObject startMenu;
+	float bright;
+	float soundVolume;
+	float musicVolume;
+
+	public Text brightText;
+	public Text soundText;
+	public Text musicText;
+
 	GameObject mainMenu;
-	GameObject title;
-	GameObject exitMenu;
 
 	void Awake()
 	{
 		thinkAnim = GameObject.FindGameObjectWithTag ("ThinkMenu").GetComponent<Animator> ();
-		startMenu = GameObject.FindGameObjectWithTag ("StartMenu");
 		mainMenu = GameObject.FindGameObjectWithTag ("MainMenu");
-		title = GameObject.FindGameObjectWithTag ("TitleMenu");
-		exitMenu = GameObject.FindGameObjectWithTag ("ExitMenu");
 
 		mainMenu.SetActive (false);
-		exitMenu.SetActive (false);
+
+		bright = 50;
+		soundVolume = 50;
+		musicVolume = 50;
+	}
+
+	void Update()
+	{
+		brightText.text = bright + "%";
+		soundText.text = soundVolume + "%";
+		musicText.text = musicVolume + "%";
+
+
+		//RenderSettings.ambientLight = new Color (bright, bright, bright, 1.0f);
 	}
 
 
 	public void pulsaEspacio()
 	{
-		startMenu.SetActive (false);
-		title.SetActive (false);
-
-
 		thinkAnim.SetBool ("Activated", true);
 
 		Invoke ("activeMenu", 1.7f);
-
-	}
-
-	public void activaExit()
-	{
-		mainMenu.SetActive (false);
-		exitMenu.SetActive (true);
-
-	}
-
-	public void desactivaExit()
-	{
-		exitMenu.SetActive (false);
-		mainMenu.SetActive (true);
 	}
 
 	void activeMenu()
 	{
 		mainMenu.SetActive(true);
+	}
+
+	public void alternaFullscreen()
+	{
+		Screen.fullScreen = !Screen.fullScreen;
+	}
+
+	public void setBright(float brillo)
+	{
+		bright = brillo;
+	}
+
+	public void setSoundVolume(float volume)
+	{
+		soundVolume = volume;
+	}
+
+	public void setMusicVolume(float volume)
+	{
+		musicVolume = volume;
+	}
+
+	public void quitGame()
+	{
+		Application.Quit ();
 	}
 }
---
+++ Chromaturgia master/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+
+	public GameObject pauseMenu;
+	public EventSystem eventSystemPauseMenu;
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+		{
+			if (GameIsPaused)
+				resume ();
+			else
+				pause ();
+		}
+	}
+
+	public void resume()
+	{
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1;
+		GameIsPaused = false;
+	}
+
+	void pause()
+	{
+		EventSystem.current = eventSystemPauseMenu;
+		pauseMenu.SetActive (true);
+		Time.timeScale = 0;
+		GameIsPaused = true;
+	}
+
+	public void loadMainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
