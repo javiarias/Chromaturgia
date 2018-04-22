94f28fcb3fa5767c4bd8dfaf37646c93b88bb558
Antnav01 <antnav01@ucm.es>
2018-04-22 20:30:53 +0200

    Creación script Door Behaviour

---
+++ Chromaturgia/Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorBehaviour : MonoBehaviour {
+	public string Scene = "Puzle 1";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") 
+		{
+			SceneManager.LoadScene (Scene);
+		}
+	}
+}
