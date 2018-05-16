e9a2af0af35a2cc1253cb919a0ee88f9320207f7
argarc03 <argarc03@ucm.es>
2018-05-16 17:04:17 +0200

    Editado FX del menú y creada escena de pre-puzle 0-0

---
+++ Chromaturgia/Assets/Scripts/IntroText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroText : MonoBehaviour {
+
+	TextMesh text;
+	string sentence;
+
+	void Start () {
+		text = GetComponent<TextMesh> ();
+		sentence = text.text;
+		text.text = "";
+		StartCoroutine (TypeSentence (sentence));
+	}
+	
+	IEnumerator TypeSentence(string sentence)
+	{
+		text.text = "";
+		foreach(char letter in sentence.ToCharArray())
+		{
+			text.text += letter;
+			FindObjectOfType<AudioManager>().Play("Talk");
+			if (letter == ',' || letter == '.' || letter == '?' || letter == '!')
+				yield return new WaitForSeconds (0.2f);
+			else
+				yield return new WaitForSeconds (0.03f);;
+		}
+	}
+}
