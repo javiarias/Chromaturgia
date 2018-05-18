d485980a573f99b9393e56ffd494f0c2624bdb4d
javiarias <javari02@ucm.es>
2018-05-18 11:45:06 +0200

    aa

--- Chromaturgia/Assets/Scripts/Menu/PauseMenu.cs
+++ Chromaturgia/Assets/Scripts/Menu/PauseMenu.cs
@@ -6,27 +6,28 @@ using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEngine.Audio;
 
-public class PauseMenu : MonoBehaviour {
+public class PauseMenu : MonoBehaviour
+{
 
-	public AudioMixer audioMixer;
+    public AudioMixer audioMixer;
 
-	public static bool GameIsPaused = false;
+    public static bool GameIsPaused = false;
 
-	public GameObject pauseMenu;
-	public EventSystem eventSystemPauseMenu;
-	public Button volverOptionsMenu;
+    public GameObject pauseMenu;
+    public EventSystem eventSystemPauseMenu;
+    public Button volverOptionsMenu;
 
-	bool needsBrightnessUpdate, needsSaturationUpdate, needsSoundUpdate, needsMusicUpdate;
+    bool needsBrightnessUpdate, needsSaturationUpdate, needsSoundUpdate, needsMusicUpdate;
 
-	float sliderBrightnessValue;
-	float sliderSaturationValue;
-	float soundVolume;
-	float musicVolume;
+    float sliderBrightnessValue;
+    float sliderSaturationValue;
+    float soundVolume;
+    float musicVolume;
 
-	public Text brightText;
-	public Text saturationText;
-	public Text soundText;
-	public Text musicText;
+    public Text brightText;
+    public Text saturationText;
+    public Text soundText;
+    public Text musicText;
 
     public Slider brightnessSlider;
     public Slider saturationSlider;
@@ -35,35 +36,35 @@ public class PauseMenu : MonoBehaviour {
 
     EventSystem eventsystem;
 
-	void Awake()
-	{
-		needsBrightnessUpdate = true;
-		needsSaturationUpdate = true;
+    void Awake()
+    {
+        needsBrightnessUpdate = true;
+        needsSaturationUpdate = true;
         needsSoundUpdate = true;
         needsMusicUpdate = true;
-	}
+    }
 
-	void Start()
-	{
+    void Start()
+    {
         sliderBrightnessValue = (GameManager.instance.brightness * 50) + 50;
         brightnessSlider.value = sliderBrightnessValue;
 
-		sliderSaturationValue = (GameManager.instance.saturation * 50) + 50;
+        sliderSaturationValue = (GameManager.instance.saturation * 50) + 50;
         saturationSlider.value = sliderSaturationValue;
 
-		soundVolume = (GameManager.instance.soundVolume - 80);
+        soundVolume = (GameManager.instance.soundVolume - 80);
         soundSlider.value = soundVolume;
 
-		musicVolume = (GameManager.instance.musicVolume - 80);
+        musicVolume = (GameManager.instance.musicVolume - 80);
         musicSlider.value = musicVolume;
     }
 
-	void Update()
-	{
-		brightText.text = sliderBrightnessValue + "%";
-		saturationText.text = sliderSaturationValue + "%";
-		soundText.text = soundVolume+80 + "%";
-		musicText.text = musicVolume+80 + "%";
+    void Update()
+    {
+        brightText.text = sliderBrightnessValue + "%";
+        saturationText.text = sliderSaturationValue + "%";
+        soundText.text = soundVolume + 80 + "%";
+        musicText.text = musicVolume + 80 + "%";
 
         if (GameIsPaused)
         {
@@ -74,109 +75,109 @@ public class PauseMenu : MonoBehaviour {
             }
         }
 
-        if (Input.GetKeyDown (KeyCode.Escape))
-		{
-			if (eventSystemPauseMenu.enabled)
-			{
-				if (GameIsPaused)
-					resume ();
-				else
-					pause ();
-			} 
-			else if (volverOptionsMenu.isActiveAndEnabled)
-			{
-				volverOptionsMenu.onClick.Invoke ();
-				FindObjectOfType<AudioManager> ().Play ("MenuBack");
-			}
-		}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (eventSystemPauseMenu.enabled)
+            {
+                if (GameIsPaused)
+                    resume();
+                else
+                    pause();
+            }
+            else if (volverOptionsMenu.isActiveAndEnabled)
+            {
+                volverOptionsMenu.onClick.Invoke();
+                FindObjectOfType<AudioManager>().Play("MenuBack");
+            }
+        }
 
-		if(GameIsPaused && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow)))
-			FindObjectOfType<AudioManager> ().Play ("MenuSelect");
+        if (GameIsPaused && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow)))
+            FindObjectOfType<AudioManager>().Play("MenuSelect");
 
-		if (needsBrightnessUpdate)
-		{
-			GameManager.instance.ChangeBrightness();
-			needsBrightnessUpdate = false;
-		}
+        if (needsBrightnessUpdate)
+        {
+            GameManager.instance.ChangeBrightness();
+            needsBrightnessUpdate = false;
+        }
 
-		if (needsSaturationUpdate)
-		{
-			GameManager.instance.ChangeSaturation();
-			needsSaturationUpdate = false;
-		}
-	}
+        if (needsSaturationUpdate)
+        {
+            GameManager.instance.ChangeSaturation();
+            needsSaturationUpdate = false;
+        }
+    }
 
-	public void setBright(float brillo)
-	{
-		sliderBrightnessValue = brillo;
-		needsBrightnessUpdate = true;
-
-		GameManager.instance.brightness = (sliderBrightnessValue - 50) / 50;
-		SaveLoad.instance.brightness = GameManager.instance.brightness;
-        SaveLoad.instance.SaveConfig();
-	}
-
-	public void setSaturation(float saturation)
-	{
-		sliderSaturationValue = saturation;
-		needsSaturationUpdate = true;
-
-		GameManager.instance.saturation = (sliderSaturationValue - 50) / 50;
-		SaveLoad.instance.saturation = GameManager.instance.saturation;
-        SaveLoad.instance.SaveConfig();
-	}
-
-	public void setSoundVolume(float volume)
-	{
-		/*soundVolume = volume;
+    public void setBright(float brillo)
+    {
+        sliderBrightnessValue = brillo;
         needsBrightnessUpdate = true;
-		audioMixer.SetFloat ("Volume",volume);
+
+        GameManager.instance.brightness = (sliderBrightnessValue - 50) / 50;
+        SaveLoad.instance.brightness = GameManager.instance.brightness;
+        SaveLoad.instance.SaveConfig();
+    }
+
+    public void setSaturation(float saturation)
+    {
+        sliderSaturationValue = saturation;
+        needsSaturationUpdate = true;
+
+        GameManager.instance.saturation = (sliderSaturationValue - 50) / 50;
+        SaveLoad.instance.saturation = GameManager.instance.saturation;
+        SaveLoad.instance.SaveConfig();
+    }
+
+    public void setSoundVolume(float volume)
+    {
+        soundVolume = volume;
+        needsBrightnessUpdate = true;
+        audioMixer.SetFloat("Volume", volume);
 
         GameManager.instance.soundVolume = (soundVolume - 50) / 50;
         SaveLoad.soundVolume = GameManager.instance.soundVolume;
-        SaveLoad.instance.SaveConfig();*/
-	}
+        SaveLoad.instance.SaveConfig();
+    }
 
-	public void setMusicVolume(float volume)
-	{
-		/*musicVolume = volume;
+    public void setMusicVolume(float volume)
+    {
+        musicVolume = volume;
         needsBrightnessUpdate = true;
 
         GameManager.instance.musicVolume = (musicVolume - 50) / 50;
         SaveLoad.instance.musicVolume = GameManager.instance.musicVolume;
-        SaveLoad.instance.SaveConfig();*/
-	}
+        SaveLoad.instance.SaveConfig();
+    }
 
-	public void resume()
-	{
-		FindObjectOfType<AudioManager> ().Play ("PausaOff");
-		pauseMenu.SetActive (false);
-		Time.timeScale = 1;
-		GameIsPaused = false;
-	}
+    public void resume()
+    {
+        FindObjectOfType<AudioManager>().Play("PausaOff");
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
 
-	public void resetLevel()
-	{
-		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
-		Time.timeScale = 1;
-		GameIsPaused = false;
-	}
+    public void resetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
 
-	void pause()
-	{
-		FindObjectOfType<AudioManager> ().Play ("PausaOn");
-		EventSystem.current = eventSystemPauseMenu;
-		pauseMenu.SetActive (true);
-		Time.timeScale = 0;
-		GameIsPaused = true;
-	}
+    void pause()
+    {
+        FindObjectOfType<AudioManager>().Play("PausaOn");
+        EventSystem.current = eventSystemPauseMenu;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
 
-	public void loadMainMenu()
-	{
-		//Time.timeScale = 1;
-		//GameIsPaused = false;
-		//SceneManager.LoadScene ("MainMenu");
-	}
+    public void loadMainMenu()
+    {
+        Time.timeScale = 1;
+        GameIsPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
 
     public void alternaFullscreen()
     {
